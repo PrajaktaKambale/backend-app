28e67aa37f0281c399eb962ac011beac01e5e5a4
PrajaktaKambale <prajaktakamble42@gmail.com>
2025-04-13 01:25:53 +0530

    APiError,Apiesponse and asyncHandler utilities done

---
+++ src/utils/asynHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+/* const asyncHandler = () => {};
+const asyncHandler = (fun) => () => {}; //higher order function
+const asyncHandler = (fun) => async () => {}; */
+
+//Using try-catch
+/* const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}; */
